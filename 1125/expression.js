96c6b889bce5f3d65bf4db2a979f3c905c0a530e
kirnjiyun <kimjiyunee@naver.com>
2023-11-27 16:07:35 +0900

    object강의

--- 1125/expression.js
+++
@@ -1,7 +0,0 @@
-// function name(){}
-// const name = function (){}
-let add = function (a, b) {
-  return a + b;
-};
-
-console.log(add(1, 2));
---
+++ 1127/create.js
@@ -0,0 +1,7 @@
+function Fruit(name, emoji) {
+  this.name = name;
+  this.emoji = emoji;
+  this.display = () => {
+    console.log(  this.name:this.emoji);
+  };
+}
